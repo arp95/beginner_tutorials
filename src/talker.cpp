1c9310000bc4df65a5ce7299ac7c27350e051c13
Arpit Aggarwal <arpitdec5@gmail.com>
2019-10-31 21:40:22 -0400

    Modified talker.cpp file

--- src/talker.cpp
+++ src/talker.cpp
@@ -34,34 +34,41 @@ int main(int argc, char **argv) {
 talker.advertise<std_msgs::String>("chatter", 1000);
     ros::Rate loopRate(10);
 
-    // Sample Logger Levels.
-    ROS_INFO_STREAM("INFO Logger Level.");
-    ROS_DEBUG_STREAM("DEBUG Logger Level.");
-    ROS_WARN_STREAM("WARN Logger Level.");
-    ROS_ERROR_STREAM("ERROR Logger Level.");
-    ROS_FATAL_STREAM("FATAL Logger Level.");
-
+    // Use Logger Levels WARN, ERROR, FATAL, DEBUG, INFO.
+    if (param == "warn") {
+        ROS_WARN_STREAM("WARN Logger Level.");
+    } else if (param == "error") {
+        ROS_ERROR_STREAM("ERROR Logger Level.");
+    } else if (param == "fatal") {
+        ROS_FATAL_STREAM("FATAL Logger Level.");
+    } else if (param == "debug") {
+        ROS_DEBUG_STREAM("DEBUG Logger Level.");
+    } else {
+        ROS_INFO_STREAM("INFO Logger Level.");
+    }
+    
     // Advertise service.
     ros::ServiceServer service = talker.advertiseService("add_two_ints", add);
     ROS_INFO_STREAM("Service ready.");
 
     // Publish INFO messages.
+    int count = 0;
     while (ros::ok()) {
         std_msgs::String message;
         std::stringstream ss;
         ROS_INFO_STREAM("Entered Loop.");
 
-        // Use Logger Levels WARN, ERROR, FATAL, DEBUG, INFO.
-        if (param == "warn") {
+        // Use Logger Levels on the basis of count variable.
+        if (count%3 == 0) {
             ss << "WARN";
             ROS_WARN_STREAM("WARN Logger Level.");
-        } else if (param == "error") {
+        } else if (count%5 == 0) {
             ss << "ERROR";
             ROS_ERROR_STREAM("ERROR Logger Level.");
-        } else if (param == "fatal") {
+        } else if (count%7 == 0) {
             ss << "FATAL";
             ROS_FATAL_STREAM("FATAL Logger Level.");
-        } else if (param == "debug") {
+        } else if (count%11 == 0) {
             ss << "DEBUG";
             ROS_DEBUG_STREAM("DEBUG Logger Level.");
         } else {
@@ -69,6 +76,7 @@ talker.advertise<std_msgs::String>("chatter", 1000);
             ROS_INFO_STREAM("INFO Logger Level.");
         }
 
+        count = count + 1;
         message.data = ss.str();
         chatterPub.publish(message);
         ros::spinOnce();
